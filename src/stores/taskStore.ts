c1c3be897273a0e677a06c15879907dfc6cde383
Oleksandr Dmytriiev <dmitrievsasa22@gmail.com>
2025-12-10 00:42:09 +0100

    feature: persist localStorage with state

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -1,24 +1,34 @@
 import type { TaskStore } from '@/types/types';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
-export const useTaskStore = create<TaskStore>()((set) => ({
-    tasks: [],
-    addTask: (body) => {
-        set(state => ({
-            tasks: [...state.tasks, { id: Date.now(), body, completed: false }]
-        }))
-    },
-    editTask: (id, newBody) => {
-        set(state => ({
-            tasks: state.tasks.map(t => t.id === id ? { ...t, body: newBody } : t)
-        }))
-    },
-    removeTask: (id) => {
-        set(state => ({ tasks: state.tasks.filter(t => t.id !== id) }))
-    },
-    toggleComplete: (id) => {
-        set(state => ({
-            tasks: state.tasks.map(t => t.id === id ? { ...t, completed: true } : t)
-        }))
-    },
-}))
+export const useTaskStore = create<TaskStore>()(
+
+    persist(
+        (set) => ({
+            tasks: [],
+            addTask: (body) => {
+                set(state => ({
+                    tasks: [...state.tasks, { id: Date.now(), body, completed: false }]
+                }))
+            },
+            editTask: (id, newBody) => {
+                set(state => ({
+                    tasks: state.tasks.map(t => t.id === id ? { ...t, body: newBody } : t)
+                }))
+            },
+            removeTask: (id) => {
+                set(state => ({ tasks: state.tasks.filter(t => t.id !== id) }))
+            },
+            toggleComplete: (id) => {
+                set(state => ({
+                    tasks: state.tasks.map(t => t.id === id ? { ...t, completed: true } : t)
+                }))
+            },
+        }),
+        {
+            name: 'tasks-storage',
+            storage: createJSONStorage(() => localStorage), //optional but I think better to specify explicitly
+        }
+    )
+)
